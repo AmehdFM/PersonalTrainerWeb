98e5a04371654791fc64010efbe63c312715b8f0
AmehdFM <amehdmendez@gmail.com>
2026-02-28 22:59:35 -0600

    Fix: Add CSRF_TRUSTED_ORIGINS for production deployment

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -27,6 +27,11 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['dsem.app', 'localhost', 'personaltrainer.dsem.app', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://personaltrainer.dsem.app',
+    'https://dsem.app',
+]
+
 
 # Application definition
 
